dabd81a46ce6ebbbe771eee35744287aa93369a3
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-01-06 10:06:29 -0300

    Add title and social icons to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
     const year = new Date().getFullYear();
 </script>
 
+<svelte:head>
+    <title>Random Quote Generator</title>
+</svelte:head>
+
 <header>
     <h1 class="title">Random Quote Generator</h1>
 </header>
@@ -13,7 +17,21 @@
         Harry Stuart Curtis "Rustic-Citrus" &copy; {year}
     </div>
     <div class="social-icons">
-        <a href="https://github.com/Rustic-Citrus">
+        <a href="https://www.harryscurtis.com/" class="social-icon">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="white"
+                class="bi bi-globe"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"
+                />
+            </svg>
+        </a>
+        <a href="https://github.com/Rustic-Citrus" class="social-icon">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -27,7 +45,10 @@
                 />
             </svg>
         </a>
-        <a href="https://www.linkedin.com/in/harrycurtis94/">
+        <a
+            href="https://www.linkedin.com/in/harrycurtis94/"
+            class="social-icon"
+        >
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -45,11 +66,12 @@
 </footer>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Caveat&family=Open+Sans:wght@300&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Caveat&family=Open+Sans:wght@300&display=swap");
 
     .title {
         font-family: "Open Sans", sans-serif;
         color: white;
+        font-size: 2rem;
     }
 
     header {
@@ -74,6 +96,8 @@
     .copyright {
         font-family: "Open Sans", sans-serif;
         color: white;
+        width: 50%;
+        text-align: center;
     }
 
     .social-icons {
@@ -82,5 +106,48 @@
         width: 50%;
         justify-content: center;
         gap: 2rem;
+    }
+
+    .social-icon:hover {
+        transform: scale(1.1);
+        transition: transform 0.4s ease-in-out;
+    }
+
+    @media only screen and (max-width: 320px) {
+        .title {
+            font-size: 1.2rem;
+        }
+
+        .copyright {
+            font-size: 0.8rem;
+            text-align: left;
+            width: 60%;
+        }
+
+        .social-icons {
+            width: 40%;
+            gap: 1.5rem;
+        }
+    }
+
+    @media only screen and (min-width: 320px) and (max-width: 576px) {
+        .title {
+            font-size: 1.4rem;
+        }
+
+        .copyright {
+            font-size: 0.8rem;
+            text-align: left;
+        }
+    }
+
+    @media only screen and (min-width: 576px) and (max-width: 768px) {
+        .title {
+            font-size: 1.8rem;
+        }
+
+        .copyright {
+            font-size: 0.9rem;
+        }
     }
 </style>
